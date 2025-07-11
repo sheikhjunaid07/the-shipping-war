2342a301e3fbaacb9360ba65a925c0a3e794c57a
sheikhjunaid07 <sheikhjunaid732000@gmail.com>
2025-07-12 00:41:00 +0530

    refector code

--- UI/src/components/UserPanel/UserPanel/UserPanel.jsx
+++ UI/src/components/UserPanel/UserPanel/UserPanel.jsx
@@ -8,40 +8,34 @@ function UserPanel() {
         <div className="slider_bg_box">
           <img src="/images/user-bg.jpg" alt="" />
         </div>
-        <div
-          id="customCarousel1"
-          className="carousel slide"
-          data-ride="carousel"
-        >
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-7">
-                    <div className="detail-box">
-                      <h1>
-                        Welcome back,{" "}
-                        <span>{localStorage.getItem("name")}</span>
-                      </h1>
-                      <h3 style={{ fontWeight: "600" }}>
-                        Your Partner for{" "}
-                        <span style={{ color: "#17a2b8" }}>Fast Shipping</span>
-                      </h3>
-                      <br />
-                      <h5>
-                        We connect you with a network of verified transporters
-                        to simplify your freight process with speed,
-                        transparency, and full visibility.
-                      </h5>
-                      <p>
-                        Say goodbye to hidden costs and delays — start managing
-                        your shipments smarter today.
-                      </p>
-                      <div className="btn-box">
-                        <a className="btn1">
-                          <Link to="/addproduct"> Get In Touch </Link>
-                        </a>
-                      </div>
+
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-7">
+                  <div className="detail-box">
+                    <h1>
+                      Welcome back, <span>{localStorage.getItem("name")}</span>
+                    </h1>
+                    <h3 style={{ fontWeight: "600" }}>
+                      Your Partner for{" "}
+                      <span style={{ color: "#17a2b8" }}>Fast Shipping</span>
+                    </h3>
+                    <br />
+                    <h5>
+                      We connect you with a network of verified transporters to
+                      simplify your freight process with speed, transparency,
+                      and full visibility.
+                    </h5>
+                    <p>
+                      Say goodbye to hidden costs and delays — start managing
+                      your shipments smarter today.
+                    </p>
+                    <div className="btn-box">
+                      <a className="btn1">
+                        <Link to="/addproduct"> Get In Touch </Link>
+                      </a>
                     </div>
                   </div>
                 </div>
