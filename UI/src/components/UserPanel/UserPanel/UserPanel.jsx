dfe3d6e3cbc31fedff824c81951a482f0848c35b
sheikhjunaid07 <sheikhjunaid732000@gmail.com>
2025-07-12 00:43:52 +0530

    refector code

--- UI/src/components/UserPanel/UserPanel/UserPanel.jsx
+++ UI/src/components/UserPanel/UserPanel/UserPanel.jsx
@@ -4,7 +4,41 @@ import { Link } from "react-router-dom";
 function UserPanel() {
   return (
     <>
-      <section className="slider_section">
+      <section className="about_section layout_padding-bottom">
+        <div>
+          <img src="/images/user-bg.jpg" alt="" />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="detail-box">
+                <div className="heading_container">
+                  <h2>
+                    Welcome back, <span>{localStorage.getItem("name")}</span>
+                  </h2>
+                </div>
+                <br />
+                <h3 style={{ fontWeight: "600" }}>
+                  Your Partner for{" "}
+                  <span style={{ color: "#17a2b8" }}>Fast Shipping</span>
+                </h3>
+                <br />
+                <h5>
+                  We connect you with a network of verified transporters to
+                  simplify your freight process with speed, transparency, and
+                  full visibility.
+                </h5>
+                <p>
+                  Say goodbye to hidden costs and delays — start managing your
+                  shipments smarter today.
+                </p>
+                <button>Start Booking</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* <section className="slider_section">
         <div className="slider_bg_box">
           <img src="/images/user-bg.jpg" alt="" />
         </div>
@@ -15,9 +49,9 @@ function UserPanel() {
               <div className="row">
                 <div className="col-md-7">
                   <div className="detail-box">
-                    <h1>
+                    <h2>
                       Welcome back, <span>{localStorage.getItem("name")}</span>
-                    </h1>
+                    </h2>
                     <h3 style={{ fontWeight: "600" }}>
                       Your Partner for{" "}
                       <span style={{ color: "#17a2b8" }}>Fast Shipping</span>
@@ -34,7 +68,7 @@ function UserPanel() {
                     </p>
                     <div className="btn-box">
                       <a className="btn1">
-                        <Link to="/addproduct"> Get In Touch </Link>
+                        <Link to="/addproduct"> Start Booking </Link>
                       </a>
                     </div>
                   </div>
@@ -43,7 +77,7 @@ function UserPanel() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
     </>
   );
 }
