6d56407994583ac53d1395caa848cb5072e8630b
sheikhjunaid07 <sheikhjunaid732000@gmail.com>
2025-07-11 19:47:10 +0530

    add website url

--- UI/src/reqUrl.jsx
+++ UI/src/reqUrl.jsx
@@ -1,9 +1,12 @@
-export const __userapiurl = "http://localhost:3001/user/";
+export const __userapiurl = "https://the-shipping-war.onrender.com/user/";
 
-export const __categoryapiurl = "http://localhost:3001/category/";
+export const __categoryapiurl =
+  "https://the-shipping-war.onrender.com/category/";
 
-export const __subcategoryapiurl = "http://localhost:3001/subcategory/";
+export const __subcategoryapiurl =
+  "https://the-shipping-war.onrender.com/subcategory/";
 
-export const __shipmentapiurl = "http://localhost:3001/shipment/";
+export const __shipmentapiurl =
+  "https://the-shipping-war.onrender.com/shipment/";
 
-export const __bidapiurl = "http://localhost:3001/bid/";
+export const __bidapiurl = "https://the-shipping-war.onrender.com/bid/";
