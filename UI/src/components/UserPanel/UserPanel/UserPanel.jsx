cb2bbc2e2df033fb91a5f2615d9774f912bfbd70
sheikhjunaid07 <sheikhjunaid732000@gmail.com>
2025-07-12 13:34:02 +0530

    update user panel

--- UI/src/components/UserPanel/UserPanel/UserPanel.jsx
+++ UI/src/components/UserPanel/UserPanel/UserPanel.jsx
@@ -46,44 +46,61 @@ function UserPanel() {
       </section> */}
       <section className="slider_section">
         <div className="slider_bg_box">
-          <img src="/images/user-bg.jpg" alt="" />
+          <img src="/images/user-panel-bg.jpg" alt="" />
         </div>
-
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-7">
-                  <div className="detail-box">
-                    <h2>
-                      Welcome back, <span>{localStorage.getItem("name")}</span>
-                    </h2>
-                    <h3 style={{ fontWeight: "600", color: "white" }}>
-                      Your Gateway to{" "}
-                      <span style={{ color: "#17a2b8" }}>
-                        Any Destination in the World.
-                      </span>
-                    </h3>
-                    <br />
-                    <h5>
-                      We connect you with a network of verified transporters to
-                      simplify your freight process with speed, transparency,
-                      and full visibility.
-                    </h5>
-                    <p>
-                      Say goodbye to hidden costs and delays — start managing
-                      your shipments smarter today.
-                    </p>
-                    <div className="btn-box">
-                      <a className="btn1">
-                        <Link to="/addproduct"> Start Booking </Link>
-                      </a>
+        <div
+          id="customCarousel1"
+          className="carousel slide"
+          data-ride="carousel"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-7">
+                    <div className="detail-box">
+                      <div className="heading_container">
+                        <h2>
+                          Welcome back,{" "}
+                          <span>{localStorage.getItem("name")}</span>
+                        </h2>
+                      </div>
+                      <h3 style={{ fontWeight: "600" }}>
+                        Your Gateway to{" "}
+                        <span style={{ color: "#17a2b8" }}>
+                          Any Destination in the World.
+                        </span>
+                      </h3>
+                      <br />
+                      <h5>
+                        We connect you with a network of verified transporters
+                        to simplify your freight process with speed,
+                        transparency, and full visibility.
+                      </h5>
+                      <p>
+                        Say goodbye to hidden costs and delays — start managing
+                        your shipments smarter today.
+                      </p>
+                      <div className="btn-box">
+                        <a className="btn1">
+                          <Link to="/addproduct"> Get In Touch </Link>
+                        </a>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          <ol className="carousel-indicators">
+            <li
+              data-target="#customCarousel1"
+              data-slide-to="0"
+              className="active"
+            ></li>
+            <li data-target="#customCarousel1" data-slide-to="1"></li>
+            <li data-target="#customCarousel1" data-slide-to="2"></li>
+          </ol>
         </div>
       </section>
     </>
