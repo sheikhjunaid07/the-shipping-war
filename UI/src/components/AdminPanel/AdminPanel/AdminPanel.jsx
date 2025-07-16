66209c381b5ba69b4ea31ac66310a3277852286c
sheikhjunaid07 <sheikhjunaid732000@gmail.com>
2025-07-16 18:00:14 +0530

    admin panel code updated

--- UI/src/components/AdminPanel/AdminPanel/AdminPanel.jsx
+++ UI/src/components/AdminPanel/AdminPanel/AdminPanel.jsx
@@ -1,6 +1,5 @@
 import "./AdminPanel.css";
 import { useEffect, useState } from "react";
-// import { Link} from "react-router-dom";
 import axios from "axios";
 import { __shipmentapiurl, __userapiurl } from "../../../reqUrl";
 import Card from "../AdminUtilities/Card";
@@ -80,29 +79,6 @@ function AdminPanel() {
           </div>
         </div>
       </section>
-      {/* <section className="about_section layout_padding-bottom">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <div className="detail-box">
-                <div className="heading_container">
-                  <h2>
-                    Welcome back, <span>{localStorage.getItem("name")}</span>
-                  </h2>
-                </div>
-                <p>
-                  There are many variations of passages of Lorem Ipsum
-                  available, but the majority have suffered alteration in some
-                  form, by injected humour, or randomized words which don't look
-                  even slightly believable. If you are going to use a passage of
-                  Lorem Ipsum, you need to be sure there isn't anything
-                  embarrassing hidden in the middle of text. All
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section> */}
     </>
   );
 }
