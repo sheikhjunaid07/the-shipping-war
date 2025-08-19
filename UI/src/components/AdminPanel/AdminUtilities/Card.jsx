63c9ec4c8be64aeefe0aa2ff0832cb7a6024e29d
sheikhjunaid07 <sheikhjunaid732000@gmail.com>
2025-08-19 13:38:58 +0530

    refactor code

--- UI/src/components/AdminPanel/AdminUtilities/Card.jsx
+++ UI/src/components/AdminPanel/AdminUtilities/Card.jsx
@@ -1,19 +1,19 @@
 import "./Card.css";
 
-const Card = (props) => {
+const Card = ({ icon, title, data }) => {
   return (
     <>
       <div className="card" style={{ width: "18rem" }}>
         <div className="card-body border border-gray">
           <h5 className="card-title ">
-            {props.icon}
-            {props.title}
+            {icon}
+            {title}
           </h5>
           <h1
             className="card-subtitle mb-2 text-body-primary"
             style={{ color: "#17a2b8", fontSize: "42px" }}
           >
-            {props.data}
+            {data}
           </h1>
         </div>
       </div>
