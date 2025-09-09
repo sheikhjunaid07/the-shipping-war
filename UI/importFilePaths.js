a446b0dbc2dd815b05ba82a5ff67d4c91d9b4861
sheikhjunaid07 <sheikhjunaid732000@gmail.com>
2025-09-09 21:44:21 +0530

    add path file

---
+++ UI/importFilePaths.js
@@ -0,0 +1,65 @@
+import Home from "./src/components/VisitorPanel/Home/Home";
+import Service from "./src/components/VisitorPanel/Service/Service";
+import About from "./src/components/VisitorPanel/About/About";
+import Contact from "./src/components/VisitorPanel/Contact/Contact";
+import Register from "./src/components/Authentication/Register/Register";
+import Login from "./src/components/Authentication/Login/Login";
+import Header from "./src/components/VisitorPanel/Header/Header";
+import AdminPanel from "./src/components/AdminPanel/AdminPanel/AdminPanel";
+import UserPanel from "./src/components/UserPanel/UserPanel/UserPanel";
+import Logout from "./src/components/Authentication/Logout/Logout";
+import CPAdmin from "./src/components/AdminPanel/CPAdmin/CPAdmin";
+import EPAdmin from "./src/components/AdminPanel/EPAdmin/EPAdmin";
+import ManageUser from "./src/components/AdminPanel/ManageUser/ManageUser";
+import AddCategory from "./src/components/AdminPanel/AddCategory/AddCategory";
+import AddSubCategory from "./src/components/AdminPanel/AddSubCategory/AddSubCategory";
+import Search from "./src/components/UserPanel/Search/Search";
+import VerifyUser from "./src/components/Authentication/VerifyUser/VerifyUser";
+import SearchSubCategory from "./src/components/UserPanel/SearchSubcategory/SearchSubCategory";
+import AddProduct from "./src/components/UserPanel/AddProduct/AddProduct";
+import ShowProducts from "./src/components/UserPanel/ShowProducts/ShowProducts";
+import EditCategory from "./src/components/AdminPanel/EditCategory/EditCategory";
+import ShowAllProducts from "./src/components/UserPanel/ShowAllProducts/ShowAllProducts";
+import EditSubCategory from "./src/components/AdminPanel/EditSubCategory/EditSubCategory";
+import ManageProducts from "./src/components/AdminPanel/ManageProducts/ManageProducts";
+import BidProduct from "./src/components/UserPanel/BidProduct/BidProduct";
+import MyProduct from "./src/components/UserPanel/MyProduct/MyProduct";
+import Auth from "./src/components/Authentication/Auth/Auth";
+import Slider from "./src/components/VisitorPanel/Slider/Slider";
+import TrackShipping from "./src/components/VisitorPanel/TrackShipping/TrackShipping";
+import Footer from "./src/components/VisitorPanel/Footer/Footer";
+import Info from "./src/components/VisitorPanel/Info/Info";
+
+export {
+  Home,
+  Service,
+  About,
+  Contact,
+  Register,
+  Login,
+  Header,
+  AdminPanel,
+  UserPanel,
+  Logout,
+  CPAdmin,
+  EPAdmin,
+  ManageUser,
+  AddCategory,
+  AddSubCategory,
+  Search,
+  VerifyUser,
+  SearchSubCategory,
+  AddProduct,
+  ShowProducts,
+  EditCategory,
+  ShowAllProducts,
+  EditSubCategory,
+  ManageProducts,
+  BidProduct,
+  MyProduct,
+  Auth,
+  Slider,
+  TrackShipping,
+  Footer,
+  Info,
+};
